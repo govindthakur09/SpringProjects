a763b51e44b2f64b12103c5c5da0776aac894ce2
Govind Thakur <govind.thakur143@gmail.com>
2018-09-15 17:30:05 +0100

    Spring project Webservice

---
+++ src/main/java/com/spring/rest/SpringRestWebService/SpringRestWebServiceApplication.java
@@ -0,0 +1,12 @@
+package com.spring.rest.SpringRestWebService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestWebServiceApplication.class, args);
+	}
+}
